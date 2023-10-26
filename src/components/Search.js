8304e1a2d921caa94ba97ba8af01e901cd1d7eb9
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-10-26 11:02:40 -0300

    Improved Search Component and Form Handling

--- src/components/Search.js
+++ src/components/Search.js
@@ -15,9 +15,15 @@ function Search({ onSearch }) {
     }
   };
 
+  // Manipulador para envio de formulário com Enter
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleSearch();
+  };
+
   return (
-    <div className="App-search">
-      <Form>
+    <div className="App-search mb-3">
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="d-flex">
           <Form.Control
             type="text"
@@ -31,7 +37,16 @@ function Search({ onSearch }) {
             onClick={handleSearch}
             disabled={searchQuery.trim() === ""}
           >
-            Search
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-search"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
           </Button>
         </Form.Group>
       </Form>
