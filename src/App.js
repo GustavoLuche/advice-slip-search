49b0711da8dd5b13d1ca1bc529558ff861712e80
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-10-28 09:06:23 -0300

    Added AdviceContext using Context API and useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/Header";
 import Greeting from "./components/Greeting";
 import Search from "./components/Search";
@@ -8,56 +8,24 @@ import SearchInfo from "./components/SearchInfo";
 import Paginator from "./components/Paginator";
 import AdviceList from "./components/AdviceList.js";
 import Footer from "./components/Footer";
-import { searchAdviceByTerm } from "./services/adviceService";
-import "./App.css"
+import { useAdviceContext } from "./context/AdviceContext";
+import "./App.css";
 
 function App() {
-  // Inicialização dos estados
-  const [adviceList, setAdviceList] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-  const [searchPerformed, setSearchPerformed] = useState(false);
-  const itemsPerPage = 5;
-
-  // Função para lidar com a pesquisa de conselhos
-  const handleSearch = async (term) => {
-    try {
-      setIsLoading(true);
-      setSearchTerm(term);
-      const adviceData = await searchAdviceByTerm(term);
-      setAdviceList(adviceData);
-      setError(null);
-      setSearchPerformed(true);
-    } catch (err) {
-      setAdviceList([]);
-      setError(err.message);
-      setSearchPerformed(false);
-    } finally {
-      setIsLoading(false);
-      setCurrentPage(1);
-    }
-  };
-
-  // Função para lidar com a mudança de página
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
+  // Usando o contexto para acessar o estado e funções
+  const { state, searchAdviceByTerm, handlePageChange } = useAdviceContext();
+  const { adviceList, error, isLoading, searchTerm, currentPage, searchPerformed, itemsPerPage } = state;
 
   return (
     <div className="App">
       <Header title="Advice Slip Search" />
       <Greeting />
       <div className={`App-container container ${searchPerformed ? "" : "no-search-performed"}`}>
-        <Search onSearch={handleSearch} />
+        <Search onSearch={searchAdviceByTerm} />
         {isLoading && <SpinnerLoading />}
         {!isLoading && !error && searchTerm !== "" && (
           <>
-            <SearchInfo
-              totalResults={adviceList.length}
-              searchTerm={searchTerm}
-            />
+            <SearchInfo totalResults={adviceList.length} searchTerm={searchTerm} />
             <Paginator
               totalResults={adviceList.length}
               currentPage={currentPage}
